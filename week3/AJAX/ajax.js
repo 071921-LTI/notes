0ace8ac4499f2ea7037ec57d30b2398f8d62d95b
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-08-04 16:20:36 -0400

    wednesday AJAX

--- week3/AJAX/ajax.js
+++ week3/AJAX/ajax.js
@@ -1,11 +1,31 @@
-document.getElementById('getData').onclick = getData;
+// document.getElementById('getData').onclick = getData;
+document.getElementById('getData').addEventListener("click", getData);
+
+let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
 
 function getData() {
-    
 
+    let userInput = document.getElementById('dataInput').value;
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = receiveData;
+
+    xhr.open('GET', apiURL + userInput);
+
+    xhr.send();
+
+    function receiveData(){
+        if(xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300){
+            let response = xhr.responseText;
+            response = JSON.parse(response);
+            populateData(response);
+        }
+    }
   
 }
 
 function populateData(response) {
-
+    console.log(response);
+    // Display pokemon information in the section tag of my html document
 }

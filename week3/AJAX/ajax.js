94a5185d171e34d14c07ba1c82bc4195d8485fbf
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-08-04 17:01:03 -0400

    adding ajax example

--- week3/AJAX/ajax.js
+++ week3/AJAX/ajax.js
@@ -5,21 +5,37 @@ let apiURL = 'https://pokeapi.co/api/v2/pokemon/';
 
 function getData() {
 
+    // Retrieving the user input, in this case the pokemon id to be retrieved
     let userInput = document.getElementById('dataInput').value;
 
+    // 4 Steps to make an AJAX call
+    // 1: Create an XMLHttpRequest object
     let xhr = new XMLHttpRequest();
 
+    // 2: Set a callback function to the readystatechange event
     xhr.onreadystatechange = receiveData;
 
-    xhr.open('GET', apiURL + userInput);
+    // 3: Open the request/parameterize it
+    xhr.open('GET', `${apiURL}${userInput}`);
 
+    // 4: Send the request
     xhr.send();
 
     function receiveData(){
-        if(xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300){
-            let response = xhr.responseText;
-            response = JSON.parse(response);
-            populateData(response);
+        let dataSection = document.getElementById('data');
+
+        // Ready state is done, reponse has been retrieved
+        if(xhr.readyState === 4){
+            // Status code is in the 200s, meaning successful
+            if(xhr.status >= 200 && xhr.status < 300){
+                let response = xhr.responseText;
+                // Converting JSON to JS object
+                response = JSON.parse(response);
+                // Data processing behavior
+                populateData(response);
+            } else{
+                document.getElementById('data').innerHTML = "<img src=https://ih1.redbubble.net/image.731955024.9007/farp,small,wall_texture,product,750x1000.u3.jpg />";
+            }
         }
     }
   
@@ -28,4 +44,35 @@ function getData() {
 function populateData(response) {
     console.log(response);
     // Display pokemon information in the section tag of my html document
+
+    let dataSection = document.getElementById('data');
+   
+   // Resets the innerHTML before loading new data
+   dataSection.innerHTML ='';
+
+   let nameTag = document.createElement('h3');
+   nameTag.innerHTML = response.name.toUpperCase();
+   
+   let abilitiesArray = response.abilities;
+   let abilities = document.createElement('ul');
+   // Appending list elements to a ul
+   for(let ability of abilitiesArray){
+       let abilityLi = document.createElement('li');
+       abilityLi.innerHTML = ability.ability.name;
+       abilities.appendChild(abilityLi);
+   }
+
+   // Appending h3 and List to the section tag
+   dataSection.appendChild(nameTag);
+   dataSection.appendChild(abilities);
+
+   // Appending sprites to section
+   let spritesObject = response.sprites;
+   for(let sprite in spritesObject){
+       if(spritesObject[sprite] && spritesObject[sprite].length > 2){
+           let spriteImg = document.createElement('img');
+           spriteImg.src = spritesObject[sprite];
+           dataSection.appendChild(spriteImg);
+       }
+   }
 }

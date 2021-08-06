e3392af598be6c255235bfb1a5a9ddfad006d46a
unknown <Lntinfotech@INFEDL31185.lntinfotech.com>
2021-08-06 09:50:56 -0500

    commit

--- week3/auth-demo/src/main/java/com/revature/daos/UserPostgres.java
+++ week3/auth-demo/src/main/java/com/revature/daos/UserPostgres.java
@@ -1,16 +1,44 @@
 package com.revature.daos;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.User;
+import com.revature.util.ConnectionUtil;
 
 public class UserPostgres implements UserDao {
 
     @Override
     public User getUserById(int id) throws UserNotFoundException {
-        // TODO Auto-generated method stub
-        return null;
+        String sql = "Select * from users where id = ?";
+        User user = null;
+        try (Connection con = ConnectionUtil.getConnectionFromEnv()) {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, id);
+
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                int userId = rs.getInt("id");
+                String username = rs.getString("username");
+                String password = rs.getString("password");
+                String role = rs.getString("role");
+
+                user = new User(userId, username, password, role);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if (user == null) {
+            throw new UserNotFoundException();
+        }
+        
+        return user;
     }
 
     @Override

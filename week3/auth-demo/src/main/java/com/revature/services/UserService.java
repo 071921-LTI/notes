abd554e4e4dd940c331fd7093071ae7b60005c6b
dvellerlti <david.veller@lntinfotech.com>
2021-08-06 10:49:00 -0400

    user service implementation

--- week3/auth-demo/src/main/java/com/revature/services/UserService.java
+++ week3/auth-demo/src/main/java/com/revature/services/UserService.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import java.util.List;
 
+import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.User;
 
 public interface UserService {
@@ -9,9 +10,9 @@ public interface UserService {
 	// Should return true if a user was successfully added
 	boolean addUser(User user);
 	List<User> getUsers();
-	User getUserById(int id);
-	User getUserByUsername(String username);
+	User getUserById(int id) throws UserNotFoundException;
+	User getUserByUsername(String username) throws UserNotFoundException;
 	// Should return true if a user was successfully added
-	boolean deleteUser(int id);
+	boolean deleteUser(int id) throws UserNotFoundException;
 	
 }
--- week3/auth-demo/src/main/java/com/revature/services/UserServiceImpl.java
+++ week3/auth-demo/src/main/java/com/revature/services/UserServiceImpl.java
@@ -2,37 +2,44 @@ package com.revature.services;
 
 import java.util.List;
 
+import com.revature.daos.UserDao;
+import com.revature.daos.UserPostgres;
+import com.revature.exceptions.UserNotFoundException;
 import com.revature.models.User;
 
 public class UserServiceImpl implements UserService{
+	
+	private UserDao ud = new UserPostgres();
 
 	@Override
 	public boolean addUser(User user) {
-		// TODO Auto-generated method stub
-		return false;
+		int id = ud.addUser(user);
+		if (id == 0) {
+			return false;
+		}
+		return true;
 	}
 
 	@Override
 	public List<User> getUsers() {
-		// TODO Auto-generated method stub
-		return null;
+		return ud.getUsers();
 	}
 
 	@Override
-	public User getUserById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public User getUserById(int id) throws UserNotFoundException {
+		return ud.getUserById(id);
 	}
 
 	@Override
-	public User getUserByUsername(String username) {
-		// TODO Auto-generated method stub
-		return null;
+	public User getUserByUsername(String username) throws UserNotFoundException {
+		return ud.getUserByUsername(username);
 	}
 
 	@Override
-	public boolean deleteUser(int id) {
-		// TODO Auto-generated method stub
+	public boolean deleteUser(int id) throws UserNotFoundException {
+		if (ud.deleteUser(id) == 1) {
+			return true;
+		}
 		return false;
 	}
 

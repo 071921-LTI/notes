5a3a81d938a3ca3c1281b2bb8b20e2db9fb63be9
Kevin Tran Huu <kevin.tranhuu@revature.com>
2021-08-19 16:00:14 -0400

    adding thursday

---
+++ week5/angular-test/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'angular-test';
+  bool: boolean = true;
+
+  toggle(): void{
+    this.bool = !this.bool;
+  }
+}
+
+
+
